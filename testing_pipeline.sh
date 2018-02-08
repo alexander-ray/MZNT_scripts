ea1fffaf9cff9255361ddf37ec94453c6d068d63
alexander-ray <1alexray@gmail.com>
2018-02-08 13:17:05 -0700

    Removed matlab script location hardcode

--- testing_pipeline.sh
+++ testing_pipeline.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # $1 is base name of bin files
 # Bin files must be at same level or lower in directory structure
-# $2 is path to SDR code directory
+# $2 is path to matlab executable
+# $3 is path to SDR code directory
 
+# Directory should be created by inotify script
 #mkdir ${1}
 mkdir ./${1}/unpacked
 touch ./${1}/${1}_log
@@ -13,6 +15,4 @@ find . -type f -name "${1}.bin*" -print0 | sort -z | xargs -0 cat -- >> "./${1}/
 echo "Running unpacker" >> ./${1}/${1}_log
 ./unpacker "./${1}/combined.bin" "./${1}/unpacked/output.bin" 0 >> ./${1}/${1}_log 2>&1
 
-#matlab_cmd="addpath(genpath('${2}')); init('${pwd}/${1}/unpacked/output.bin.GPS_L1'); exit();"
-#/Applications/MATLAB_R2016b.app/bin/matlab -nodisplay -nodesktop -r $matlab_cmd 
-/Applications/MATLAB_R2016b.app/bin/matlab -nodisplay -nodesktop -r "addpath(genpath('${2}')); init('$(pwd)/${1}/unpacked/output.bin.GPS_L1'); exit();" >> ./${1}/${1}_log 2>&1
+${2} -nodisplay -nodesktop -r "addpath(genpath('${3}')); init('$(pwd)/${1}/unpacked/output.bin.GPS_L1'); exit();" >> ./${1}/${1}_log 2>&1
