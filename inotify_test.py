d7a4f393186eebb39bc62b4325df2632595ac21d
alexander-ray <1alexray@gmail.com>
2018-02-20 12:13:40 -0700

    Updated files from gnssfast testing

--- inotify_test.py
+++ inotify_test.py
@@ -1,50 +1,95 @@
 import os
 import sys
 import subprocess
-
+import threading
+import re
+from time import sleep
 # Pip3 install inotify_simple
 from inotify_simple import INotify, flags
-'''
-# Make subdir for IF files
-if len(sys.argv) > 1:
-    path = "./" + sys.argv[1]
-    os.mkdir(path)
-else:
-    print("Pass base name of IF log files")
-    exit(1)
-'''
 
-inotify = INotify()
-watch_flags = flags.CREATE
-wd = inotify.add_watch('./', watch_flags)
+def worker(mz_dir):
+    inotify = INotify()
+    #watch_flags = flags.CREATE | flags.MODIFY | flags.MOVED_TO
+    watch_flags = flags.CREATE
+    # Assume directory has been created if thread has spawned
+    wd = inotify.add_watch(mz_dir, watch_flags) 
+    
+    # Once I get one, make sure it's "pre" and add to dictionary
+    # If I have a filename that's been added to dict, double check that it's "post"
+    # mkdir with base name, move both files in as well as .sh (time?)
+    # Unpack, concat, run matlab (should all be script)
+    #POST_BYTES = 20000000
+    POST_BYTES = 499646464
+    # Keys will be mz1_preData.*
+    path_dict = {}
 
-# Maintain dictionary of names
-# Once I get one, make sure it's "pre" and add to dictionary
-# If I have a filename that's been added to dict, double check that it's "post"
-# mkdir with base name, move both files in as well as .sh (time?)
-# Unpack, concat, run matlab (should all be script)
-path_dict = {}
+    while True:
+        '''
+        Names are in the following format:
+        mz1.IF.bin, mz1.IF.bin.1.bin, mz1.IF.bin.2.bin, etc for periodic IF
+        mz1_preData.0.bin and mz1_postData.0.bin for RFI event
+             mz1_preData.0.bin.0 and mz1_preData.0.bin.1 for fragmented pre files
+        '''
+        # And see the corresponding events:
+        for event in inotify.read():
+            name = event.name
+            # Split name on both periods and underscores
+            name_list = re.split(r'[._]+', name)
+            # Simple sanity check
+            if 'Data' not in name_list[1]:
+                continue
 
-while True:
-    # And see the corresponding events:
-    for event in inotify.read():
-        name_list = event.name.split('.')
-        if name_list[0] != 'microzed':
-            continue
+            # key is filename, without extension & pre/post
+            key = name_list[0] + '_' + 'data.' + name_list[2]
+            if (key in path_dict) and ('post' in name):
+                # Dir name is node_data.*
+                dir_name = key
+                subpath = mz_dir + dir_name
+                os.system('mkdir ' + subpath)
+               
+                # While post hasn't been fully written, sleep
+                while os.stat(mz_dir + name).st_size < POST_BYTES:
+                    sleep(0.5)
+                
+                path_dict[key].append(name)
+                # Move all files with this naming convention into new directory
+                # mv should be atomic
+                for f in path_dict[key]:
+                    os.system('mv ' + mz_dir + f + ' ' + subpath)
+                
+                # cp scripts into new directory
+                os.system('cp testing_pipeline.sh ' + subpath + '/test.sh')
+                os.system('cp concatfiles.sh ./' +  subpath + '/concatfiles.sh')
+                os.system('cp unpacker ./' + subpath + '/unpacker')
+                
+                # Code to call shell script with command line arguments
+                subprocess.Popen('./test.sh ' + key + ' ' + sys.argv[1] + ' ' +  sys.argv[2], shell=True, cwd=subpath)
+            # another pre file
+            elif (key in path_dict) and ('pre' in name):
+                path_dict[key].append(name)
+            # First pre file, add key to dict, append full filename
+            elif key not in path_dict:
+                path_dict[key] = []
+                path_dict[key].append(name)
 
-	# name_list[2] is pre/post
-        if name_list[0] in path_dict and name_list[2] == 'post':
-            # Dir name is concatenated pre timestamp
-            dir_name = path_dict[name_list[0]] + '_' + name_list[0] 
+def main():
+    # Simple arg check
+    if len(sys.argv) <= 2:
+        print("Pass path to matlab executable and path to SDR code")
+        exit(1)
+    subdirs = []
+    subdirs.append('./mz1/test3/')
+    subdirs.append('./mz2/test3/')
+    subdirs.append('./mz3/test3/')
+    #mz4_t = './mz4/test3/'
+    #mz5_t = './mz5/test3/'
+    #mz6_t = './mz6/test3/'
+    threads = []
+    for d in subdirs:
+        t = threading.Thread(target=worker, args=(d,))
+        threads.append(t)
+        t.start()
 
-            os.system('mkdir ' + dir_name)
-            # Move all files with this naming convention into new directory
-            os.system('mv ' + name_list[0] + '* ./' + dir_name)
-            # cp shell script into new directory
-            os.system('cp testing_pipeline.sh ./' + dir_name)
-            # CALL TESTING PIPELINE SCRIPT
-        elif name_list[0] not in path_dict and name_list[2] == 'pre':
-            path_dict[name_list[0]] = name_list[1]
 
-# Code to call shell script with command line arguments
-#subprocess.call(['./test.sh', 'why', 'helloworld'])
+if __name__ == '__main__':
+    main()
--- testing_pipeline.sh
+++ testing_pipeline.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+#/bin/bash
 # $1 is base name of bin files
 # Bin files must be at same level or lower in directory structure
 # $2 is path to matlab executable
 # $3 is path to SDR code directory
 
 # Directory should be created by inotify script
-#mkdir ${1}
-mkdir ./${1}/unpacked
-touch ./${1}/${1}_log
+# Currently in correct subdirectory
+mkdir ./unpacked
+touch ./${1}_log
 
-echo "Combining IF files" >> ./${1}/${1}_log
-find . -type f -name "${1}.bin*" -print0 | sort -z | xargs -0 cat -- >> "./${1}/combined.bin"
+echo "Combining IF files" >> ./${1}_log
+find . -type f -name "*.bin*" -print0 | sort -z | xargs -0 cat -- >> "./combined.bin"
 
-echo "Running unpacker" >> ./${1}/${1}_log
-./unpacker "./${1}/combined.bin" "./${1}/unpacked/output.bin" 0 >> ./${1}/${1}_log 2>&1
+echo "Running unpacker" >> ./${1}_log
+./unpacker "./combined.bin" "./unpacked/output.bin" 0 >> ./${1}_log 2>&1
 
-${2} -nodisplay -nodesktop -r "addpath(genpath('${3}')); init('$(pwd)/${1}/unpacked/output.bin.GPS_L1'); exit();" >> ./${1}/${1}_log 2>&1
+matlab -nodisplay -nodesktop -r "addpath(genpath('${3}')); init('$(pwd)/unpacked/output.bin.GPS_L1'); exit();" >> ./${1}_log 2>&1
